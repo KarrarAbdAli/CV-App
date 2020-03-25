e557fae7b7f6d00f3ec767105a807ecb2c56f1f0
Karrar Abd Ali <>
2020-03-25 23:48:57 +0100

    Adding Models

---
+++ CV App/Models/Experience.swift
@@ -0,0 +1,28 @@
+//
+//  Experience.swift
+//  CV App
+//
+//  Created by Karrar Abd Ali on 25/03/2020.
+//  Copyright © 2020 Karrar Abd Ali. All rights reserved.
+//
+
+import Foundation
+class Experience{
+    var jobTitle: String
+    var company: String?
+    var startDate: Date
+    var endDate: Date
+    var city: String
+    var country: String
+    var items: [String]
+    
+    init(jobTitle: String, company: String?, startDate: Date,endDate: Date, city: String, country: String, items: [String]){
+        self.jobTitle = jobTitle
+        self.company = company
+        self.startDate = startDate
+        self.endDate = endDate
+        self.city = city
+        self.country = country
+        self.items = items
+    }
+}
