26fd3a5b8ad40ef15916e3bc47bbf6cea2bf94db
Karrar Abd Ali <>
2020-03-25 22:59:55 +0100

    starting the project

---
+++ CV App/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CV App
+//
+//  Created by Karrar Abd Ali on 25/03/2020.
+//  Copyright © 2020 Karrar Abd Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CV App/Models/TitleField.swift
@@ -0,0 +1,37 @@
+//
+//  TitleField.swift
+//  CV App
+//
+//  Created by Karrar Abd Ali on 25/03/2020.
+//  Copyright © 2020 Karrar Abd Ali. All rights reserved.
+//
+
+import Foundation
+class TitleField{
+    let nameAndSurname: String
+    var jobTitle: String?
+    var email: String?
+    var phoneNumber: Int?
+    var address: String?
+    var linkedIn: String?
+    var skype: String?
+    var git: String?
+    init(nameAndSurname: String,
+         jobTitle: String? = nil,
+         email: String?,
+         phoneNumber: Int? = nil,
+         address: String? = nil,
+         linkedIn: String? = nil,
+         skype: String? = nil,
+         git: String? = nil) {
+        
+        self.nameAndSurname = nameAndSurname
+        self.jobTitle = jobTitle
+        self.email = email
+        self.phoneNumber = phoneNumber
+        self.address = address
+        self.linkedIn = linkedIn
+        self.skype = skype
+        self.git = git
+    }
+}
